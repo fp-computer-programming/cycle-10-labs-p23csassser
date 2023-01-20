d67a2355a0552d471f14b2b8a235fdfcbf46f2b0
Colin Sasser <p23csasser@fairfieldprep.org>
2023-01-20 13:43:33 -0500

    submission

---
+++ Lab_10-1.py
@@ -0,0 +1,11 @@
+#Creator CS 1/20/2023
+
+# Initialize sum variable
+sum = 0
+
+# Use while loop to contune asking user for input 
+while True:
+    # Ask user for input
+    num = int(input("Enter a number: "))
+    
+   
